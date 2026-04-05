5a664ee5d846f829a7a61a5ceb0fdedef50bbe26
Avya Nand <avyanand54@gmail.com>
2026-04-05 18:25:38 +0530

    Update no results message to use h3 tag

--- script.js
+++ script.js
@@ -9,18 +9,26 @@ function searchPG() {
   );
 
   if (filtered.length === 0) {
-    resultsDiv.innerHTML = "<p>No PG Found 😢</p>";
+    resultsDiv.innerHTML = "<h3 style='text-align:center;'>No PG Found 😢</h3>";
     return;
   }
 
   filtered.forEach(pg => {
     resultsDiv.innerHTML += `
       <div class="card">
+        <img src="${pg.image}" />
         <h3>${pg.name}</h3>
         <p>📍 ${pg.city}</p>
         <p>💰 ${pg.price}</p>
         <p>📞 ${pg.contact}</p>
+        <a href="https://www.google.com/maps/search/${pg.name}" target="_blank">
+          📍 View Location
+        </a>
       </div>
     `;
   });
+}
+
+function chat() {
+  alert("Try searching: PG in Bhopal under ₹5000 😊");
 }
