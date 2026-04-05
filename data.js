860b73052f14a26f1c830fd6fe924c6e49d9798a
Avya Nand <avyanand54@gmail.com>
2026-04-05 18:24:42 +0530

    Add image URLs and update hostel names in data.js

--- data.js
+++ data.js
@@ -3,18 +3,21 @@ const pgData = [
     name: "Sharma PG",
     city: "Bhopal",
     price: "₹5000",
-    contact: "9876543210"
+    contact: "9876543210",
+    image: "https://images.unsplash.com/photo-1560448204-e02f11c3d0e2"
   },
   {
     name: "Luxury Boys Hostel",
     city: "Bhopal",
     price: "₹7000",
-    contact: "9123456780"
+    contact: "9123456780",
+    image: "https://images.unsplash.com/photo-1555854877-bab0e564b8d5"
   },
   {
-    name: "Girls PG Comfort",
+    name: "Girls Comfort PG",
     city: "Pune",
     price: "₹8000",
-    contact: "9988776655"
+    contact: "9988776655",
+    image: "https://images.unsplash.com/photo-1505691938895-1758d7feb511"
   }
 ];
