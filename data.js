b72df260644303aa6ea4da9d0c50584f50d4b2c7
Avya Nand <avyanand54@gmail.com>
2026-04-05 18:35:12 +0530

    Add multiple hostels data to data.js

--- data.js
+++ data.js
@@ -14,10 +14,49 @@ const pgData = [
     image: "https://images.unsplash.com/photo-1555854877-bab0e564b8d5"
   },
   {
+    name: "City Boys Hostel",
+    city: "Bhopal",
+    price: "₹6000",
+    contact: "9988776655",
+    image: "https://images.unsplash.com/photo-1505691938895-1758d7feb511"
+  },
+  {
+    name: "Green Valley PG",
+    city: "Bhopal",
+    price: "₹4500",
+    contact: "9090909090",
+    image: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688"
+  },
+  {
+    name: "Comfort Stay Hostel",
+    city: "Bhopal",
+    price: "₹6500",
+    contact: "9012345678",
+    image: "https://images.unsplash.com/photo-1493809842364-78817add7ffb"
+  },
+
+  // 🔥 TERA REAL ENTRY (IMAGE WAALI)
+  {
+    name: "Shri Ram Boys Hostel & Guest House",
+    city: "Bhopal",
+    price: "₹5000 (Approx)",
+    contact: "8123869239",
+    image: "https://images.unsplash.com/photo-1560448204-e02f11c3d0e2"
+  },
+
+  // EXTRA CITY (GROWTH START)
+  {
     name: "Girls Comfort PG",
     city: "Pune",
     price: "₹8000",
-    contact: "9988776655",
-    image: "https://images.unsplash.com/photo-1505691938895-1758d7feb511"
+    contact: "8877665544",
+    image: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267"
+  },
+  {
+    name: "Pune City Hostel",
+    city: "Pune",
+    price: "₹7000",
+    contact: "7766554433",
+    image: "https://images.unsplash.com/photo-1505693416388-ac5ce068fe85"
   }
 ];
