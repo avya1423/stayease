1f653da72b15e79f05beb6a5f7532e39c615bf9a
Avya Nand <avyanand54@gmail.com>
2026-04-05 17:57:49 +0530

    Add pgData array with hostel information

---
+++ data.js
@@ -0,0 +1,20 @@
+const pgData = [
+  {
+    name: "Sharma PG",
+    city: "Bhopal",
+    price: "₹5000",
+    contact: "9876543210"
+  },
+  {
+    name: "City Boys Hostel",
+    city: "Bhopal",
+    price: "₹6000",
+    contact: "9123456780"
+  },
+  {
+    name: "Girls Comfort PG",
+    city: "Pune",
+    price: "₹7000",
+    contact: "9988776655"
+  }
+];
