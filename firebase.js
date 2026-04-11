bf233579b0fbf1645a20a0558f78057f98e02bd1
Avya Nand <avyanand54@gmail.com>
2026-04-11 16:06:56 +0530

    Fix formatting of firebase configuration object

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyABdEEMZ_Q52P5E01Cy2PPmKAAXvHnTetQ",
-  authDomain: "pgkhojo-1180e.firebaseapp.com",
-  projectId: "pgkhojo-1180e",
-  storageBucket: "pgkhojo-1180e.appspot.com",
-  messagingSenderId: "976145591846",
-  appId: "1:976145591846:web:be2ecc09a15225eb4d80e7"
+  apiKey:"AIzaSyABdEEMZ_Q52P5E01Cy2PPmKAAXvHnTetQ",
+  authDomain:"pgkhojo-1180e.firebaseapp.com",
+  projectId:"pgkhojo-1180e",
+  storageBucket:"pgkhojo-1180e.appspot.com",
+  messagingSenderId:"976145591846",
+  appId:"1:976145591846:web:be2ecc09a15225eb4d80e7"
 };
 
 firebase.initializeApp(firebaseConfig);
