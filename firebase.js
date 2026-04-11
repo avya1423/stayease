075a04169a91af162a8d1ab15f1fc2c68768839b
Avya Nand <avyanand54@gmail.com>
2026-04-11 15:19:52 +0530

    Initialize Firebase with configuration settings

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyABdEEMZ_Q52P5E01Cy2PPmKAAXvHnTetQ",
+  authDomain: "pgkhojo-1180e.firebaseapp.com",
+  projectId: "pgkhojo-1180e"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const auth = firebase.auth();
