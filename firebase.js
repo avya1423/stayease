83bafc1c90f7209f47d649cd6bd4a24ea1d42b10
Avya Nand <avyanand54@gmail.com>
2026-04-11 15:44:46 +0530

    Fix formatting in firebase.js configuration

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyABdEEMZ_Q52P5E01Cy2PPmKAAXvHnTetQ",
-  authDomain: "pgkhojo-1180e.firebaseapp.com",
-  projectId: "pgkhojo-1180e"
+  apiKey:"AIzaSyABdEEMZ_Q52P5E01Cy2PPmKAAXvHnTetQ",
+  authDomain:"pgkhojo-1180e.firebaseapp.com",
+  projectId:"pgkhojo-1180e"
 };
 
 firebase.initializeApp(firebaseConfig);
