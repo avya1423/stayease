5a3ed79116f0aa9ee8e765d6e1b3a9205d2f05c3
Avya Nand <avyanand54@gmail.com>
2026-04-25 00:45:38 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,164 @@
+const pgData = [
+
+  // BHOPAL
+  {
+    name: "Shri Ram Boys Hostel & Guest House",
+    city: "Bhopal",
+    price: "₹5000",
+    contact: "8123869239",
+    image: "https://images.unsplash.com/photo-1560448204-e02f11c3d0e2",
+    gender: "Boys"
+  },
+  {
+    name: "City Boys Hostel",
+    city: "Bhopal",
+    price: "₹6000",
+    contact: "9876543210",
+    image: "https://images.unsplash.com/photo-1505691938895-1758d7feb511",
+    gender: "Boys"
+  },
+  {
+    name: "Lake View Girls PG",
+    city: "Bhopal",
+    price: "₹6500",
+    contact: "9111111111",
+    image: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688",
+    gender: "Girls"
+  },
+  {
+    name: "MP Nagar Comfort PG",
+    city: "Bhopal",
+    price: "₹5500",
+    contact: "9222222222",
+    image: "https://images.unsplash.com/photo-1505693416388-ac5ce068fe85",
+    gender: "Boys"
+  },
+
+  // PATNA 🔥
+  {
+    name: "Patna City Boys PG",
+    city: "Patna",
+    price: "₹5000",
+    contact: "9333333333",
+    image: "https://images.unsplash.com/photo-1505691938895-1758d7feb511",
+    gender: "Boys"
+  },
+  {
+    name: "Girls Comfort Patna",
+    city: "Patna",
+    price: "₹6000",
+    contact: "9444444444",
+    image: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688",
+    gender: "Girls"
+  },
+  {
+    name: "Kankarbagh Hostel",
+    city: "Patna",
+    price: "₹5500",
+    contact: "9555555555",
+    image: "https://images.unsplash.com/photo-1505693416388-ac5ce068fe85",
+    gender: "Boys"
+  },
+  {
+    name: "Patna Premium PG",
+    city: "Patna",
+    price: "₹7000",
+    contact: "9666666666",
+    image: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267",
+    gender: "Girls"
+  },
+
+  // DELHI
+  {
+    name: "Delhi Comfort PG",
+    city: "Delhi",
+    price: "₹8000",
+    contact: "9812345678",
+    image: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267",
+    gender: "Boys"
+  },
+  {
+    name: "North Delhi Boys Hostel",
+    city: "Delhi",
+    price: "₹7000",
+    contact: "9898989898",
+    image: "https://images.unsplash.com/photo-1505693416388-ac5ce068fe85",
+    gender: "Boys"
+  },
+
+  // MUMBAI
+  {
+    name: "Mumbai Stay PG",
+    city: "Mumbai",
+    price: "₹10000",
+    contact: "9765432109",
+    image: "https://images.unsplash.com/photo-1493809842364-78817add7ffb",
+    gender: "Boys"
+  },
+  {
+    name: "Andheri Girls PG",
+    city: "Mumbai",
+    price: "₹9000",
+    contact: "9988776655",
+    image: "https://images.unsplash.com/photo-1555854877-bab0e564b8d5",
+    gender: "Girls"
+  },
+
+  // PUNE
+  {
+    name: "Pune City Hostel",
+    city: "Pune",
+    price: "₹7000",
+    contact: "8877665544",
+    image: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688",
+    gender: "Boys"
+  },
+  {
+    name: "Girls Comfort PG",
+    city: "Pune",
+    price: "₹8000",
+    contact: "7766554433",
+    image: "https://images.unsplash.com/photo-1505691938895-1758d7feb511",
+    gender: "Girls"
+  },
+
+  // BANGALORE
+  {
+    name: "Bangalore Tech PG",
+    city: "Bangalore",
+    price: "₹9000",
+    contact: "9123456789",
+    image: "https://images.unsplash.com/photo-1555854877-bab0e564b8d5",
+    gender: "Boys"
+  },
+
+  // HYDERABAD
+  {
+    name: "Hyderabad Elite PG",
+    city: "Hyderabad",
+    price: "₹7500",
+    contact: "9011223344",
+    image: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267",
+    gender: "Girls"
+  },
+
+  // CHENNAI
+  {
+    name: "Chennai Comfort PG",
+    city: "Chennai",
+    price: "₹6500",
+    contact: "9090901122",
+    image: "https://images.unsplash.com/photo-1493809842364-78817add7ffb",
+    gender: "Boys"
+  },
+
+  // KOLKATA
+  {
+    name: "Kolkata Boys Hostel",
+    city: "Kolkata",
+    price: "₹6000",
+    contact: "9876512345",
+    image: "https://images.unsplash.com/photo-1505691938895-1758d7feb511",
+    gender: "Boys"
+  }
+];
